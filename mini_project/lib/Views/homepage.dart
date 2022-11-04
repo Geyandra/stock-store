0d369dcffae03ea46667536ba178ea576156fed3
geyandra <geyandra.g.k@gmail.com>
2022-11-04 10:04:32 +0700

    search bar function success

--- mini_project/lib/Views/homepage.dart
+++ mini_project/lib/Views/homepage.dart
@@ -4,10 +4,20 @@ import 'package:mini_project/Models/model_products.dart';
 import 'package:mini_project/Views/create.dart';
 import 'package:mini_project/Views/details.dart';
 
-class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+class Homepage extends StatefulWidget {
+  Homepage({super.key});
   static const nameRoute = 'Homepage';
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int? Columnindex;
+  bool asc = false;
+  List<Products> search = [];
+  String searchresult = '';
+  final nametable = ["Nama Barang", "Stock", "Satuan"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,22 +27,6 @@ class Homepage extends StatelessWidget {
         },
         child: Icon(Icons.add),
       ),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        bottom: PreferredSize(
-            preferredSize: Size.fromHeight(10),
-            child: Container(
-              margin: EdgeInsets.only(bottom: 5),
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    hintText: "Search",
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30))),
-              ),
-            )),
-      ),
       body: SingleChildScrollView(
           child: StreamBuilder<List<Products>>(
         stream: readData(),
@@ -40,35 +34,101 @@ class Homepage extends StatelessWidget {
           if (snapshot.hasError) {
             return Text("Ada Kesalahan! ${snapshot.hasError}");
           } else if (snapshot.hasData) {
-            final datas = snapshot.data!;
-            return SizedBox(
-              width: double.infinity,
-              child: DataTable(
-                  columns: <DataColumn>[
-                    DataColumn(label: Text("Name")),
-                    DataColumn(label: Text("Stock"), numeric: true),
-                    DataColumn(label: Text("Satuan")),
-                  ],
-                  rows: datas
-                      .map((data) => DataRow(
-                              onLongPress: () {
-                                Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => DetailsProduct(data: data,)),
-            );
-                              },
-                              cells: [
-                                DataCell(
-                                  Text(data.Nama_Barang),
-                                ),
-                                DataCell(
-                                  Text("${data.Jumlah_Barang}"),
-                                ),
-                                DataCell(
-                                  Text(data.Tipe),
-                                ),
-                              ]))
-                      .toList()),
+            List<Products> datas = snapshot.data!;
+            return Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 5, top: 10),
+                  width: double.infinity,
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        search = datas
+                            .where((element) =>
+                                element.Nama_Barang.toLowerCase()
+                                    .contains(value.toLowerCase()))
+                            .toList();
+                      });
+                    },
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.blue.shade100,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none),
+                      hintText: "Search Data",
+                      hintStyle: TextStyle(color: Colors.blue),
+                      prefixIcon: Icon(Icons.search),
+                      prefixIconColor: Colors.blue,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: DataTable(
+                      sortColumnIndex: Columnindex,
+                      sortAscending: asc,
+                      columns: nametable
+                          .map((String column) => DataColumn(
+                                label: Text(column),
+                                onSort: (columnIndex, ascending) {
+                                  setState(() {
+                                    Columnindex = columnIndex;
+                                    asc = ascending;
+                                  });
+                                  if (columnIndex == 0) {
+                                    if (asc) {
+                                      datas.sort(
+                                        (a, b) => a.Nama_Barang.compareTo(
+                                            b.Nama_Barang),
+                                      );
+                                    } else {
+                                      datas.sort(
+                                        (a, b) => b.Nama_Barang.compareTo(
+                                            a.Nama_Barang),
+                                      );
+                                    }
+                                  } else if (columnIndex == 1) {
+                                    if (asc) {
+                                      datas.sort(
+                                        (a, b) => a.Jumlah_Barang.compareTo(
+                                            b.Jumlah_Barang),
+                                      );
+                                    } else {
+                                      datas.sort(
+                                        (a, b) => b.Jumlah_Barang.compareTo(
+                                            a.Jumlah_Barang),
+                                      );
+                                    }
+                                  }
+                                },
+                              ))
+                          .toList(),
+                      rows: search
+                          .map((data) => DataRow(
+                                  onLongPress: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => DetailsProduct(
+                                                data: data,
+                                              )),
+                                    );
+                                  },
+                                  cells: [
+                                    DataCell(
+                                      Text(data.Nama_Barang),
+                                    ),
+                                    DataCell(
+                                      Text("${data.Jumlah_Barang}"),
+                                    ),
+                                    DataCell(
+                                      Text(data.Tipe),
+                                    ),
+                                  ]))
+                          .toList()),
+                ),
+              ],
             );
           } else {
             return Center(
